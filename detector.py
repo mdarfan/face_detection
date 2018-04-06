7fc71cf9111a68b96f7cb6f5b2c5698909a1a8ba
Mohammed Arfan <35273346+mdarfan@users.noreply.github.com>
2018-04-06 23:25:40 +0530

    Add files via upload

---
+++ detector.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
+eyeDetect=cv2.CascadeClassifier('haarcascade_eye.xml');
+cam=cv2.VideoCapture(0);
+rec=cv2.createLBPHFaceRecognizer();
+rec.load("recognizer\\trainingData.yml")
+id=0
+font =cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_COMPLEX_SMALL,5,1,0,4)
+while(True):
+    ret,img=cam.read();
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=faceDetect.detectMultiScale(gray,1.3,5);
+    eye=eyeDetect.detectMultiScale(gray,1.3,5);
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        id,conf=rec.predict(gray[y:y+h,x:x+w])
+        if(id==1):
+            id="MD arfan"
+        elif(id==2):
+            id="Rahul"
+
+        elif(id==3):
+            id=" vade"
+        elif(id==4):
+            id="swagath thanya"   
+        cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,255);
+                   
+    for(x,y,w,h) in eye:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+    cv2.imshow("Face",img);
+    if(cv2.waitKey(1)==ord('q')):
+        break;
+cam.release()
+cv2.destroyAllWindows()
